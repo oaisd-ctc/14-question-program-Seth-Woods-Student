64a7d94718124ade8a4251720a84d8b80aecda3a
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-09 14:44:53 +0000

    almost done

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using system;
+
+namespace 14questions {
+    class program {
+        static void Main(string[] args) {
+                    Console.WriteLine("Hello! What is your name?");
+            string name = Console.readline();
+                    Console.WriteLine("what is your age?");
+            int age = Console.readline();
+                    Console.WriteLine("what is your eye color?");
+            string eyecolor = Console.readline();
+                    Console.WriteLine("what is your hair color?");
+            string haircolor = Console.readline();
+                    Console.WriteLine("what is your shoe size?");
+            string shoesize = Console.readline();
+                    Console.WriteLine("what is your favorite color?");
+            string color = Console.readline();
+                    Console.WriteLine("What is your favorite movie?");
+            string movie = Console.readline();
+                    Console.WriteLine("what is your favorite teacher?");
+            string teacher = Console.readline();
+                    Console.WriteLine("what is your favorite class?");
+            string class = Console.readline();
+                    Console.WriteLine("what is your favorite holiday?");
+            string holiday = Console.readline();
+                    Console.WriteLine("what is your favorite season?");
+            string season = Console.readline();
+                    Console.WriteLine("Hello! What is your name?");
+            string name = Console.readline();
+                    Console.WriteLine("Hello! What is your name?");
+            string name = Console.readline();
+                    Console.WriteLine("Hello! What is your name?");
+            string name = Console.readline();
+            
+        }
+    }
+}
+
