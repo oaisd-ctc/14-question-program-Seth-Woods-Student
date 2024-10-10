132f8e9f87ed5028b64d90b49029c34ac8a2d900
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-10 14:40:31 +0000

    fin

--- Program.cs
+++ Program.cs
@@ -1,38 +1,53 @@
-﻿using system;
+﻿using System;
 
-namespace 14questions {
-    class program {
-        static void Main(string[] args) {
-                    Console.WriteLine("Hello! What is your name?");
-            string name = Console.readline();
-                    Console.WriteLine("what is your age?");
-            int age = Console.readline();
-                    Console.WriteLine("what is your eye color?");
-            string eyecolor = Console.readline();
-                    Console.WriteLine("what is your hair color?");
-            string haircolor = Console.readline();
-                    Console.WriteLine("what is your shoe size?");
-            string shoesize = Console.readline();
-                    Console.WriteLine("what is your favorite color?");
-            string color = Console.readline();
-                    Console.WriteLine("What is your favorite movie?");
-            string movie = Console.readline();
-                    Console.WriteLine("what is your favorite teacher?");
-            string teacher = Console.readline();
-                    Console.WriteLine("what is your favorite class?");
-            string class = Console.readline();
+public class Program
+{
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Hello! What is your name?");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("what is your age?");
+                int ageNumber = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("what is your eye color?");
+                string eyecolor = Console.ReadLine();
+
+                Console.WriteLine("what is your hair color?");
+                string haircolor = Console.ReadLine();
+
+                Console.WriteLine("what is your shoe size?");
+                string shoesize = Console.ReadLine();
+
+                Console.WriteLine("what is your favorite color?");
+                string color = Console.ReadLine();
+
+                Console.WriteLine("What is your favorite movie?");
+                string movie = Console.ReadLine();
+
+                Console.WriteLine("what is your favorite teacher?");
+                string teacher = Console.ReadLine();
+
+                Console.WriteLine("what is your favorite class?");
+                string favoriteClass = Console.ReadLine();
+
                     Console.WriteLine("what is your favorite holiday?");
-            string holiday = Console.readline();
-                    Console.WriteLine("what is your favorite season?");
-            string season = Console.readline();
-                    Console.WriteLine("Hello! What is your name?");
-            string name = Console.readline();
-                    Console.WriteLine("Hello! What is your name?");
-            string name = Console.readline();
-                    Console.WriteLine("Hello! What is your name?");
-            string name = Console.readline();
-            
-        }
-    }
+            string holiday = Console.ReadLine();
+
+        Console.WriteLine("what is your favorite season?");
+            string season = Console.ReadLine();
+
+        Console.WriteLine("what is your dream job?");
+            string job = Console.ReadLine();
+
+        Console.WriteLine("how many siblings do you have?");
+            string sibling = Console.ReadLine();
+        Console.WriteLine("hello " + name + ". you have " + eyecolor + "eyes, " + haircolor + " hair, and your shoe size is a "
+         + shoesize + ". your favorite color is " + color + ", favorite movie " + movie + ", favorite teacher " + teacher 
+         + ", favorite class " + favoriteClass + ", favorite holiday " + holiday + ", favorite season " + season 
+         + ", and your dream job is " + job + ". You have " + sibling + "sibling" + ", and you will be " + (ageNumber + 5) + " in five years.");
+
 }
+    
+   }
 
