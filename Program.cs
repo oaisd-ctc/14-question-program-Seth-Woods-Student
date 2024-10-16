13c6407df3f7cf3bac776999cc6abe469abb35e5
Seth-Woods-Student <sewoae4@student.oaisd.org>
2024-10-16 13:45:50 +0000

    done again

--- Program.cs
+++ Program.cs
@@ -1,53 +1,116 @@
 ﻿using System;
+using System.Drawing;
 
 public class Program
 {
-        static void Main(string[] args)
-        {
-                Console.WriteLine("Hello! What is your name?");
-                string name = Console.ReadLine();
+    private static int ageNumber;
+    private static string? name;
+    private static string? eyeColor;
+    private static string? hairColor;
+    private static string? shoeSize;
+    private static string? movie;
+    private static string? teacher;
+    private static string? favoriteClass;
+    private static string? holiday;
+    private static string? season;
+    private static string? job;
+    private static string? sibling;
+    private static string? color;
 
-                Console.WriteLine("what is your age?");
-                int ageNumber = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("what is your eye color?");
-                string eyecolor = Console.ReadLine();
 
-                Console.WriteLine("what is your hair color?");
-                string haircolor = Console.ReadLine();
 
-                Console.WriteLine("what is your shoe size?");
-                string shoesize = Console.ReadLine();
+    static void Main(string[] args)
+    {
+        YourName();
+        YourEye();
+        YourAge();
+        YourHair();
+        YourShoe();
+        YourColor();
+        YourMovie();
+        YourTeacher();
+        YourClass();
+        YourHoliday();
+        YourSeason();
+        YourJob();
+        YourSiblings();
+        theEnd();
+    }
+    public static void YourName()
+    {
+        Console.WriteLine("Hello! What is Your name?");
+        name = Console.ReadLine();
+    }
+    public static void YourAge()
+    {
+        Console.WriteLine("what is Your age?");
+        ageNumber = int.Parse(Console.ReadLine());
+    }
+    public static void YourEye()
+    {
+        Console.WriteLine("what is Your eye color?");
+        eyeColor = Console.ReadLine();
+    }
+    public static void YourHair()
+    {
+        Console.WriteLine("what is Your hair color?");
+        hairColor = Console.ReadLine();
+    }
+    public static void YourShoe()
+    {
+        Console.WriteLine("what is Your shoe size?");
+        shoeSize = Console.ReadLine();
+    }
+    public static void YourColor()
+    {
+        Console.WriteLine("what is Your favorite color?");
+        color = Console.ReadLine();
 
-                Console.WriteLine("what is your favorite color?");
-                string color = Console.ReadLine();
+    }
+    public static void YourMovie()
+    {
+        Console.WriteLine("What is Your favorite movie?");
+        movie = Console.ReadLine();
+    }
+    public static void YourTeacher()
+    {
+        Console.WriteLine("what is Your favorite teacher?");
+        teacher = Console.ReadLine();
+    }
+    public static void YourClass()
+    {
+        Console.WriteLine("what is Your favorite class?");
+        favoriteClass = Console.ReadLine();
+    }
+    public static void YourHoliday()
+    {
+        Console.WriteLine("what is Your favorite holiday?");
+        holiday = Console.ReadLine();
 
-                Console.WriteLine("What is your favorite movie?");
-                string movie = Console.ReadLine();
-
-                Console.WriteLine("what is your favorite teacher?");
-                string teacher = Console.ReadLine();
-
-                Console.WriteLine("what is your favorite class?");
-                string favoriteClass = Console.ReadLine();
-
-                    Console.WriteLine("what is your favorite holiday?");
-            string holiday = Console.ReadLine();
-
-        Console.WriteLine("what is your favorite season?");
-            string season = Console.ReadLine();
-
-        Console.WriteLine("what is your dream job?");
-            string job = Console.ReadLine();
-
+    }
+    public static void YourSeason()
+    {
+        Console.WriteLine("what is Your favorite season?");
+        season = Console.ReadLine();
+    }
+    public static void YourJob()
+    {
+        Console.WriteLine("what is Your dream job?");
+        job = Console.ReadLine();
+    }
+    public static void YourSiblings()
+    {
         Console.WriteLine("how many siblings do you have?");
-            string sibling = Console.ReadLine();
-        Console.WriteLine("hello " + name + ". you have " + eyecolor + "eyes, " + haircolor + " hair, and your shoe size is a "
-         + shoesize + ". your favorite color is " + color + ", favorite movie " + movie + ", favorite teacher " + teacher 
-         + ", favorite class " + favoriteClass + ", favorite holiday " + holiday + ", favorite season " + season 
-         + ", and your dream job is " + job + ". You have " + sibling + "sibling" + ", and you will be " + (ageNumber + 5) + " in five years.");
+        sibling = Console.ReadLine();
+    }
+    public static void theEnd()
+    {
+        Console.WriteLine("hello " + name + ". you have " + eyeColor + "eyes, " + hairColor + " hair, and Your shoe size is a "
+     + shoeSize + ". Your favorite color is " + color + ", favorite movie " + movie + ", favorite teacher " + teacher
+     + ", favorite class " + favoriteClass + ", favorite holiday " + holiday + ", favorite season " + season
+     + ", and Your dream job is " + job + ". You have " + sibling + "sibling" + ", and you will be " + (ageNumber + 5) + " in five years.");
 
+    }
 }
-    
-   }
 
